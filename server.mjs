66cd4ad8c6460a472f6a565f3d862106c17dd0dc
SRowlandNCL <81402110+SRowlandNCL@users.noreply.github.com>
2021-03-26 13:57:29 +0000

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,5 +1,7 @@
 /*Server Example provided within the module found here : Practical-Web4-ServerSide Web API
-Original Author Dan Jackson Editing Author Scarlett Rowland */
+Original Author Dan Jackson Editing Author Scarlett Rowland 
+
+This code creates a server, passes the variable to the html using birdCount from /scanbird */
 
 import express from 'express'
 import path from 'path'
@@ -11,6 +13,7 @@ const app = express()
 
 app.use('/', express.static(path.join(currentFolder, 'Public')))
 
+// This enables the variable to be passed to the HTML file
 app.get('/birds', (req, res) => {
   res.json({
     birds: birdCount
@@ -22,5 +25,5 @@ console.log(path.join(currentFolder, 'Public'))
 import { birdCount} from './scanbird.mjs'
 
 app.listen(3001, () => {
-  console.log('Listening at http://localhost:3001')
+  console.log('Listening at http://localhost:3001') //Click this link/the one in the terminal to view the webpage
 })
